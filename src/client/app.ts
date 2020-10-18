21871d042f8d749defb7a85622aa24e6b3b8453b
fugel <tom@fugel.co.uk>
2020-10-18 14:33:44 +0100

    Update to use "transformRequests" in map component

--- src/client/app.ts
+++ src/client/app.ts
@@ -5,6 +5,11 @@ var map = new mapboxgl.Map({
   style: "/style/osm-bright/style.json", // style URL
   center: [-2, 54], // starting position [lng, lat]
   zoom: 5, // starting zoom
+  transformRequest: (url:any, resourceType:any) => {
+    if (/^local:\/\//.test(url)) {
+        return { url: new URL(url.substr('local://'.length), location.href).href };
+    }
+}
 });
 
 map.addControl(new mapboxgl.NavigationControl())
